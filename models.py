8ea5e114f61fafdc5f34a661209d6e99ab4416ee
xinkent <xinkent1994@gmail.com>
2017-11-01 12:57:22 +0900

    update generator

--- models.py
+++ models.py
@@ -32,8 +32,8 @@ def CBR(ch,shape,bn=True,sample='down',activation=LeakyReLU, dropout=False):
 def discriminator():
     h = 512
     w = 256
-    gen_output = Input(shape=(h,w,3))
     label_input = Input(shape=(h,w,1))
+    gen_output = Input(shape=(h,w,3))
     x1 = CBR(32,(512,256,1), bn=False)(label_input)
     x2 = CBR(32,(256,128,3),bn=False)(gen_output)
     x = concatenate([x1,x2])
@@ -41,7 +41,7 @@ def discriminator():
     x = CBR(256,(64,32,128))(x)
     x = CBR(512,(32,16,256))(x)
     x = Conv2D(filters=1,kernel_size=3,strides=1,padding='same')(x)
-    x = Activation('softplus')(x)
+    x = Activation('sigmoid')(x)
     output = Lambda(lambda x: K.mean(x, axis=[1,2]),output_shape=(1,))(x)
     model = Model(inputs =[label_input,gen_output], outputs = [output])
 
@@ -52,35 +52,40 @@ def generator():
 
     # encoder
     input1 = Input(shape=(512,256,1))
-    enc_1 = Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='same',input_shape=(256,256,1))(input1)
-    enc_2 = CBR(128,(512,256,64))(enc_1)
-    enc_3 = CBR(256,(256,128,128))(enc_2)
-    enc_4 = CBR(512,(128,64,256))(enc_3)
-    enc_5 = CBR(512,(64,32,512))(enc_4)
-    enc_6 = CBR(512,(32,16,512))(enc_5)
-    enc_7 = CBR(512,(16,8,512))(enc_6)
-    enc_8 = CBR(512,(8,4,512))(enc_7)
+    enc_1 = Conv2D(filters=16, kernel_size=(3,3), strides=1, padding='same',input_shape=(512,256,1))(input1)
+
+    input2 = Input(shape=(512,256,3))
+    enc_2 = Conv2D(filters=16, kernel_size=(3,3), strides=1, padding='same',input_shape=(512,256,1))(input1)
+
+    enc3 = concatenate([enc_1,enc_2])
+
+    enc_4 = CBR(64,(512,256,32))(enc_3)
+    enc_5 = CBR(128,(256,128,64))(enc_4)
+    enc_6 = CBR(256,(128,64,128))(enc_5)
+    enc_7 = CBR(512,(64,32,256))(enc_6)
+    enc_8 = CBR(512,(32,16,512))(enc_7)
+    enc_9 = CBR(512,(16,8,512))(enc_8)
+    enc_10 = CBR(512,(8,4,512))(enc_9)
 
     # decoder
-    x = CBR(512,(4,2,512),sample='up',activation='relu',dropout=True)(enc_8)
-    x = CBR(512,(8,4,1024),sample='up',activation='relu',dropout=True)(concatenate([x,enc_7]))
-    x = CBR(512,(16,8,1024),sample='up',activation='relu',dropout=True)(concatenate([x,enc_6]))
-    x = CBR(512,(32,16,1024),sample='up',activation='relu',dropout=False)(concatenate([x,enc_5]))
-    x = CBR(256,(64,32,1024),sample='up',activation='relu',dropout=False)(concatenate([x,enc_4]))
-
-    x = CBR(128,(128,64,512),sample='up',activation='relu',dropout=False)(concatenate([x,enc_3]))
-    x = CBR(64,(256,128,256),sample='up',activation='relu',dropout=False)(concatenate([x,enc_2]))
-    output = Conv2D(filters=3, kernel_size=(3,3),strides=1,padding="same")(concatenate([x,enc_1]))
-
+    x = CBR(512,(4,2,512),sample='up',activation='relu',dropout=True)(enc_10)
+    x = CBR(512,(8,4,1024),sample='up',activation='relu',dropout=True)(concatenate([x,enc_9]))
+    x = CBR(512,(16,8,1024),sample='up',activation='relu',dropout=True)(concatenate([x,enc_8]))
+    x = CBR(256,(32,16,1024),sample='up',activation='relu',dropout=False)(concatenate([x,enc_7]))
+    x = CBR(128,(64,32,512),sample='up',activation='relu',dropout=False)(concatenate([x,enc_6]))
+    x = CBR(64,(128,64,256),sample='up',activation='relu',dropout=False)(concatenate([x,enc_5]))
+    x = CBR(32,(256,128,128),sample='up',activation='relu',dropout=False)(concatenate([x,enc_4]))
+    output = Conv2D(filters=3, kernel_size=(3,3),strides=1,padding="same")(concatenate([x,enc_3]))
     model = Model(inputs=input1, outputs=output)
     return(model)
 
 
 def GAN(generator, discriminator):
 
-    gen_input = Input(shape=(512,256,1))
-    generated_image = generator(gen_input)
-    DCGAN_output = discriminator([gen_input,generated_image])
+    s_input = Input(shape=(512,256,1))
+    c_input = Input(shape=(512,256,3))
+    generated_image = generator([s_input,c_input])
+    DCGAN_output = discriminator([s_input,generated_image])
     DCGAN = Model(inputs=[gen_input],outputs=[generated_image, DCGAN_output],name="DCGAN")
 
     return DCGAN
