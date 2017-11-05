5f8d2632f6cfa59c2016e20a09cedc91137553c7
xinkent <xinkent@gmail.com>
2017-11-05 13:44:53 +0900

    fix

--- fish_dataset.py
+++ fish_dataset.py
@@ -63,8 +63,8 @@ def load_dataset2(dataDir='/data1/train_data1/', data_range=range(0,300),skip=Tr
             imgNum = imgStart + i
             slabelNum = slabelStart + i
             clabelNum = clabelStart + i
-            img = Image.open(dataDir + "up/train_up%05d.png"%imgNum)
-            label_color = Image.open(dataDir + "up_night/night_up%05d.png"%clabelNum)
+            img = Image.open(dataDir + "up/up%05d.png"%imgNum)
+            label_color = Image.open(dataDir + "up_25night/night_up%05d.png"%clabelNum)
             label_sonar = Image.open(dataDir + "sonar/" + "2017-03-02_105804_%d_width_773_height_1190.png"%slabelNum)
             label_sonar = label_sonar.convert("L")
 
@@ -79,12 +79,12 @@ def load_dataset2(dataDir='/data1/train_data1/', data_range=range(0,300),skip=Tr
             label_sonar = label_sonar[:,:,np.newaxis]
             label_color = np.asarray(label_color)/128.0-1.0
 
-            h,w = img.shape
+            h,w,_ = img.shape
             xl = np.random.randint(0,w-256)
             yl = np.random.randint(0,h-512)
             img = img[yl:yl+512, xl:xl+256, :]
-            label_sonar = label_sonar[yl:yl+512, xl:xl_256,:]
-            label_color = label_color[yl:yl+512, xl:xl_256,:]
+            label_sonar = label_sonar[yl:yl+512, xl:xl+256,:]
+            label_color = label_color[yl:yl+512, xl:xl+256,:]
 
             imgDataset.append(img)
             slabelDataset.append(label_sonar)
