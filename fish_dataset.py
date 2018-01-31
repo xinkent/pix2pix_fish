a7dee450d635cf3545620e756a21a5ef628bd9b6
xinkent <xinkent1994@gmail.com>
2018-01-31 23:42:53 +0900

    update

--- fish_dataset.py
+++ fish_dataset.py
@@ -117,13 +117,13 @@ def load_dataset_box(dataDir='/data1/train_data/', data_range=range(0,300),test=
         img = img[yl:yl+512, xl:xl+256, :]
         sonar = sonar[yl:yl+512, xl:xl+256,:]
 
-        box_img = np.copy(img)
-        box_size = 150
-        xl = np.random.randint(0,256 - 50)
-        yl = np.random.randint(0,512 - 50)
-        box_img[xl:xl+box_size, yl:yl+box_size] = 0
         imgDataset.append(img)
         sonarDataset.append(sonar)
+        box_img = np.copy(img)
+        box_size = 150
+        xl = np.random.randint(0,256 - box_size)
+        yl = np.random.randint(0,512 - box_size)
+        box_img[yl:yl+box_size,xl:xl+box_size] = -1
         nightDataset.append(box_img)
 
 
